440109684233ba71cc661baca38ce0589478fca2
gale2307 <nicholaswilliam2307@gmail.com>
2021-02-09 23:51:37 -0800

    Crawler update + bert
    Crawler now parses crafting recipes.
    Uploaded Bert_test for testing.

--- crawler.py
+++ crawler.py
@@ -8,6 +8,87 @@ def urltofilename(url):
     filename = filename + ".txt"
     return filename
 
+def craftingrecipe(tag):
+    #class="invslot animated" <-- animated frame
+    #class="invslot" <-- normal frame
+    #class="animated-subframe animated-active" / class="animated-subframe" <-- loop through different types of same ingredient
+    #class="invslot-item animated-active" <-- first item in loop
+    #get title in tags 'a'
+    #class="mcui-output" <-- crafting output
+    hlist = list()
+    rlist = list(list())
+    rcounter = -1 #account for header
+
+    for row in tag.find_all("tr"):
+        if rcounter < 0:
+            for header in row.find_all("th"):
+                hlist.append(str(''.join(header.findAll(text=True))))
+        else:
+            rlist.append(list())
+
+            recipelist = list(list())
+            resultlist = list()
+
+            for col in row.find_all("td"):
+                if col.has_attr("style"):
+                    for span in col.find_all("span"):
+                        tmp = list()                                                #List of ingredients for a single frame
+                        if ''.join(span["class"]) == "invslot":                     #INGREDIENT
+                            sprite = span.find_all("span", class_= "sprite inv-sprite")
+                            if not(sprite):
+                                sprite = span.find_all("a")
+                            if not(sprite):
+                                tmp.append("empty")
+                            else:
+                                for item in sprite:
+                                    tmp.append(str(item["title"]))
+                            recipelist.append(tmp)
+
+                        elif ''.join(span["class"]) == "invslotanimated":          #LOOPING INGREDIENT
+                            for item in span.find_all("span"):
+                                if "animated-subframe" in ''.join(item["class"]):   #SIMILAR ITEM IN LOOPING INGREDIENT
+                                    for sprite in item.find_all("span", class_ = "sprite inv-sprite"):
+                                        tmp.append(str(sprite["title"]))
+                                        break
+                                    for x in item.find_all("span"):
+                                        x["class"] = "done"
+                                elif "invslot-item" in ''.join(item["class"]):
+                                    for sprite in item.find_all("span", class_ = "sprite inv-sprite"):
+                                        tmp.append(str(sprite["title"]))
+                            #print(tmp)
+                            recipelist.append(tmp)
+
+                        elif ''.join(span["class"]) == "mcui-output":               #RECIPE OUTPUT
+                            for item in span.find_all("span", class_ = "sprite inv-sprite"):
+                                resultlist.append(str(item["title"]))
+
+                    if len(recipelist)>=9:
+                        recipe = ""
+                        for i in range(len(resultlist)):
+                            recipe += "The crafting recipe for " + resultlist[i] + " is"
+                            for j in range(9):
+                                if len(recipelist[j])>0:
+                                    recipe += " " + recipelist[j][i % len(recipelist[j])] + ","
+                            recipe = recipe[:-1] + ".\n"
+                        rlist[rcounter].append(recipe)
+
+                #else:
+                    #rlist[rcounter].append(str(''.join(col.findAll(text=True))))
+
+        rcounter += 1
+
+
+    #for x in hlist:
+    #    print(x)
+
+    text = ""
+    for lists in rlist:
+        for x in lists:
+            #print(x)
+            text += x
+
+    return text
+
 wiki_url = 'https://minecraft.gamepedia.com'
 my_url = 'https://minecraft.gamepedia.com/minecraft_wiki'
 
@@ -48,10 +129,16 @@ while url_list:
         continue
     html_text = r.text
     soup = BeautifulSoup(html_text, 'html.parser')
+
     #index the URL (probably can be improved)
     my_str = ""
-    for node in soup.findAll('p'):
-        my_str = my_str + str(''.join(node.findAll(text=True)))
+    for tag in soup.find_all(["p", "table"]):
+        if tag.name == "table":
+            if tag.has_attr('data-description'):
+                if tag['data-description'] == "Crafting recipes":
+                    my_str = my_str + craftingrecipe(tag)
+        else:
+            my_str = my_str + str(''.join(tag.findAll(text=True)))
         #if tag.name == li:         #how to relate it to previous text?
         #   my_str = my_str + '\n'
 
@@ -73,6 +160,3 @@ while url_list:
                     if not(new_url.lower() in stacked_url):
                         stacked_url.add(new_url.lower())
                         url_list.append(new_url)
-                        #fo.write(new_url + '\n')
-
-#fo.close()
