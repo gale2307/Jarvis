9c2ff275d506de9e8ab92a796db25e804f0cef03
anouneemous1 <anouneemous1@gmail.com>
2021-02-23 10:08:59 -0800

    general IR improvements

--- indexer.py
+++ indexer.py
@@ -1,6 +1,6 @@
 import os.path
 import whoosh.index as index
-from whoosh.analysis import StandardAnalyzer
+from whoosh.analysis import StemmingAnalyzer
 from whoosh.fields import *
 
 #List of stopwords for the analyzer applied to page content
@@ -8,7 +8,7 @@ stops = frozenset(['and', 'is', 'it', 'an', 'as', 'at', 'have', 'in', 'yet', 'if
                    'not', 'with', 'tbd', 'a', 'on', 'your', 'this', 'of', 'us', 'will', 'can', 'the', 'or', 'are', 'what', 'how', 'why'])
 
 #Creates index schema; only the title is stored, and receives increased weight
-schema = Schema(title=TEXT(stored=True,field_boost=3.0), content=TEXT(analyzer=StandardAnalyzer(stoplist=stops)), path=ID)
+schema = Schema(fullTitle=ID(stored=True), content=TEXT(analyzer=StemmingAnalyzer(stoplist=stops)), title=TEXT(analyzer=StemmingAnalyzer(stoplist=stops),field_boost=3.0))
 
 #Creates and opens index object
 if not os.path.exists("index"):
@@ -22,10 +22,11 @@ writer = ix.writer()
 for subdir, dirs, files in os.walk(r'MinecraftWiki/'):
     for filename in files:
         filepath = subdir + os.sep + filename
-        print("Processing: {}\n".format(filename))
+        short_title = filename[30:].rstrip(".txt").replace("_", " ")
+        print("Processing: {} (full title: {})\n".format(short_title, filename))
         with open(filepath, 'r', encoding="utf-8") as f:
             f_str = f.read()
-            writer.add_document(title=u"{}".format(filename), path=u"{}".format(filepath), content=u"{}".format(f_str))
+            writer.add_document(fullTitle=u"{}".format(filename), content=u"{}".format(f_str), title=u"{}".format(short_title))
             f.close()
 
 writer.commit()
--- searcher.py
+++ searcher.py
@@ -1,24 +1,27 @@
 import whoosh.index as index
+import whoosh.scoring as scoring
+import whoosh.qparser as qparser
 from whoosh.searching import Searcher
-from whoosh.qparser import QueryParser
 from whoosh.highlight import WholeFragmenter
 
 #Opens index
 ix = index.open_dir("index")
 
-#Lets the user ask a query
-query = input("JARVIS online. What would you like to know?\n")
+while (True):
+    #Lets the user ask a query
+    query = input("JARVIS online. What would you like to know?\n")
 
-#Handles query
-qp = QueryParser("content", schema=ix.schema)
-q = qp.parse(query)
+    #Handles query
+    og = qparser.OrGroup.factory(0.9)
+    qp = qparser.MultifieldParser(["title", "content"], schema=ix.schema, group=og)
+    q = qp.parse(query)
 
-#Searches index for query, returns best-matching page(s)
-with ix.searcher() as searcher:
-    results = searcher.search(q)
-    for i in range(10):
-        print(results[i]['title'])
-    #Print highlights of best-matched page; alter maxchars, surround, and top to get more relevant highlights
-    #results.fragmenter.maxchars = 100
-    #results.fragmenter.surround = 30
-    #print(results[0].highlights("content", top=4))
+    #Searches index for query, returns best-matching page(s)
+    with ix.searcher(weighting=scoring.TF_IDF()) as searcher:
+        results = searcher.search(q)
+        for i in range(10):
+            print(results[i]['fullTitle'])
+        #Print highlights of best-matched page; alter maxchars, surround, and top to get more relevant highlights
+        #results.fragmenter.maxchars = 100
+        #results.fragmenter.surround = 30
+        #print(results[0].highlights("content", top=4))
