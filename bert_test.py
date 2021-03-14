3df11ade921437974c5bfc890f7c090ace11aad7
gale2307 <nicholaswilliam2307@gmail.com>
2021-03-14 12:49:54 -0700

    Added bert eval script

--- bert_test.py
+++ bert_test.py
@@ -1,17 +1,32 @@
 from transformers import BertForQuestionAnswering
 from transformers import BertTokenizer
+from f1score import get_metric_score
 import bert_class
 
-model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
-tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')\
+model = BertForQuestionAnswering.from_pretrained('deepset/bert-large-uncased-whole-word-masking-squad2')
+tokenizer = BertTokenizer.from_pretrained('deepset/bert-large-uncased-whole-word-masking-squad2')
 
-while(True):
-    context = input("Enter Target Filename for BERT:\n")
-    if context == "exit": break
-    if context[:13] != "MinecraftWiki/": context = "MinecraftWiki/" + context
-    if context[-4:] != ".txt": context += ".txt"
-    while(True):
-        query = input("JARVIS online. What would you like to know?\n")
-        if query == "exit": break
-        answer, score = bert_class.answerfromwebpage(query, context, model, tokenizer)
-        print("Answer: " + answer)
+total_score = 0.0
+
+with open("bert_eval.txt", "r") as file:
+    for line in file:
+
+        question, context, answers = line.split('|', 2)
+        gold_answers = answers.split('|')
+
+        if context[:13] != "MinecraftWiki/": context = "MinecraftWiki/" + context
+
+        print("question: " + question)
+
+        #ask the question to the model
+        #receive predicted answer from model
+        model_answer, model_score = bert_class.answerfromwebpage(question, context, model, tokenizer)
+
+        print(gold_answers)
+        score = get_metric_score(model_answer, gold_answers)
+        #print question and score
+        print("answer: " + model_answer)
+        print("score: " + str(score))
+        total_score+=score[1]
+
+print("Total score: " + str(total_score))
