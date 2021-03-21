611eda5048e3499a1a32e711cf0292a48129b195
gale2307 <nicholaswilliam2307@gmail.com>
2021-03-20 19:17:06 -0700

    Added time counter

--- roberta_eval.py
+++ roberta_eval.py
@@ -1,8 +1,10 @@
 import whoosh.index as index
 from transformers import RobertaForQuestionAnswering, RobertaTokenizer
+import time
 from f1score import get_metric_score
 import roberta_class
 
+#model_name = "deepset/roberta-large-squad2"
 model_name = "deepset/roberta-base-squad2"
 model = RobertaForQuestionAnswering.from_pretrained(model_name)
 tokenizer = RobertaTokenizer.from_pretrained(model_name)
@@ -12,6 +14,7 @@ total_score = 0.0
 
 ix = index.open_dir("index")
 
+start_time = time.perf_counter()
 with open("eval.txt", "r") as file:
     for line in file: #add while loop
 
@@ -20,7 +23,7 @@ with open("eval.txt", "r") as file:
             answers = answers[:-1]
         gold_answers = answers.split('|')
 
-        print("question: " + question)
+        print("\nquestion: " + question)
 
         #ask the question to the model
         #receive predicted answer from model
@@ -33,4 +36,7 @@ with open("eval.txt", "r") as file:
         print("score: " + str(score))
         total_score+=score[1]
 
+end_time = time.perf_counter()
+
+print(f"Ran in {end_time - start_time:0.4f} seconds")
 print("Total score: " + str(total_score))
