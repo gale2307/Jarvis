1910492ad38fc00194513226bfbaaf373e3d28f9
gale2307 <nicholaswilliam2307@gmail.com>
2021-03-21 10:45:15 -0700

    Added model_eval to test models individually

--- model_eval.py
+++ model_eval.py
@@ -2,13 +2,27 @@ from transformers import BertForQuestionAnswering
 from transformers import BertTokenizer
 from f1score import get_metric_score
 import bert_class
+import roberta_class
+import electra_class
 
-model = BertForQuestionAnswering.from_pretrained('deepset/bert-large-uncased-whole-word-masking-squad2')
-tokenizer = BertTokenizer.from_pretrained('deepset/bert-large-uncased-whole-word-masking-squad2')
+#model_name = "bert-large-uncased-whole-word-masking-finetuned-squad"
+#model_name = "deepset/bert-large-uncased-whole-word-masking-squad2"
+#model = BertForQuestionAnswering.from_pretrained(model_name)
+#tokenizer = BertTokenizer.from_pretrained(model_name)
+
+model_name = "deepset/roberta-large-squad2"
+#model_name = "deepset/roberta-base-squad2"
+model = RobertaForQuestionAnswering.from_pretrained(model_name)
+tokenizer = RobertaTokenizer.from_pretrained(model_name)
+
+#model_name = "deepset/electra-base-squad2"
+#model_name = "ahotrod/electra_large_discriminator_squad2_512"
+#model = ElectraForQuestionAnswering.from_pretrained(model_name)
+#tokenizer = ElectraTokenizer.from_pretrained(model_name)
 
 total_score = 0.0
 
-with open("bert_eval.txt", "r") as file:
+with open("model_eval.txt", "r") as file:
     for line in file:
 
         question, context, answers = line.split('|', 2)
@@ -16,7 +30,7 @@ with open("bert_eval.txt", "r") as file:
 
         if context[:13] != "MinecraftWiki/": context = "MinecraftWiki/" + context
 
-        print("question: " + question)
+        print("\nquestion: " + question)
 
         #ask the question to the model
         #receive predicted answer from model
